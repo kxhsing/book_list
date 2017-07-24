fba73efe6e74693daf779d9437b64e43ebf4f8b6
Karen Hsing <kxhsing@gmail.com>
2017-07-24 15:15:34 -0700

    Created reducer for books

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        {title: 'We Bought A Zoo'},
+        {title: 'The Bell Jar'},
+        {title: 'Lean In'},
+        {title: 'Modern Romance'},
+    ]
+}
