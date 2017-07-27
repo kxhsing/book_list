381984b047bd84929bc98c22bba2949c8fe127b0
Karen Hsing <kxhsing@gmail.com>
2017-07-27 15:45:55 -0700

    Added action for active book and book detail container

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books.js';
+import ActiveBook from './reducer_active_book.js';
 
+
+//any key defined in combineReducers becomes available to global state
 const rootReducer = combineReducers({
-    books: BooksReducer
+    books: BooksReducer,
+    activeBook: ActiveBook
 });
 
 export default rootReducer;
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
     return [
-        {title: 'We Bought A Zoo'},
-        {title: 'The Bell Jar'},
-        {title: 'Lean In'},
-        {title: 'Modern Romance'},
+        {title: 'We Bought A Zoo', pages: 200},
+        {title: 'The Bell Jar', pages: 150},
+        {title: 'Lean In', pages: 350},
+        {title: 'Modern Romance', pages: 250},
     ]
 }
